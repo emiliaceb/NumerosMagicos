08732c45c25767a3384d566b7cf92b98a51fa94c
Emiliaceb <emiceballoss24@gmail.com>
2024-07-29 16:35:03 -0300

    agregue el form. funciona

--- js/app.js
+++ js/app.js
@@ -4,12 +4,24 @@ const comenzarJuego = () => {
     const botonComenzar = document.querySelectorAll('button');
     console.log('botonComenzar[0]');
     botonComenzar[0].style.display = 'none';
-    const parrafoNuevo = document.createElement('p');
-    parrafoNuevo.textContent = 'Ingresa un numero del 1 al 100';
 
-    //inserto el parrafo nuevo
+    const formulario = document.createElement('form');
+    //creo lo de adentro del form
+    const label = document.createElement('label');
+    label.textContent = 'Ingresa un numero del 1 al 100'
+    const input = document.createElement('input');
+    const botonEnviar = document.createElement('button');
+    botonEnviar.innerHTML = 'Enviar'
+
+    //inserto
     const div = document.querySelectorAll('div');
     console.log(div);
-    div[2].appendChild(parrafoNuevo);
+    div[2].appendChild(formulario);
+    const form = document.querySelector('form');
+    console.log(form);
+    form.appendChild(label);
+    form.appendChild(input);
+    form.appendChild(botonEnviar);
+    
 }
 
