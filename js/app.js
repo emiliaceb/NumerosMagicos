03619c381b74e7c8abe01157f893d0549c49f065
Emiliaceb <emiceballoss24@gmail.com>
2024-07-29 21:04:46 -0300

    Termine de completar funciones y logica. ejercicio terminado

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
+let numeroAleatorio;
 const comenzarJuego = () => {
-    console.log('desde la funcion comenzar juego')
+    numeroAleatorio = Math.floor(Math.random() * (100 - 1) + 1);
+    console.log(numeroAleatorio,' <= Numero aleatorio');
     //obtener el boton
     const botonComenzar = document.querySelectorAll('button');
-    console.log('botonComenzar[0]');
     botonComenzar[0].style.display = 'none';
 
     const formulario = document.createElement('form');
@@ -11,9 +12,10 @@ const comenzarJuego = () => {
     label.textContent = 'Ingresa un numero del 1 al 100'
     const input = document.createElement('input');
     const botonEnviar = document.createElement('button');
-    botonEnviar.innerHTML = 'Enviar'
+    botonEnviar.innerHTML = 'Enviar';
+    botonEnviar.className= 'buttonEnviar'
 
-    //inserto
+    //insertar
     const div = document.querySelectorAll('div');
     console.log(div);
     div[2].appendChild(formulario);
@@ -22,6 +24,45 @@ const comenzarJuego = () => {
     form.appendChild(label);
     form.appendChild(input);
     form.appendChild(botonEnviar);
-    
+
+    form.addEventListener('submit',adivinarNumero);
 }
+
+const adivinarNumero = (e) =>{
+ e.preventDefault();
+ const input = document.querySelector('input');
+ const numero = parseInt(input.value)
+ console.log('Numero ingresado:',numero);
+ let bandera = false;
+
+ if(numero<1 || numero>100 || isNaN(numero)){
+    alert('El numero ingresado no es valido.');
+    document.querySelector('input').value = '';
+ }
+ else{
+    const div = document.querySelectorAll('div');
+
+    //remover el parrafo anterior si existe
+    const parrafoAnterior = document.querySelector('#resultado');
+    if(parrafoAnterior){
+        div[2].removeChild(parrafoAnterior);
+    }
+
+    const nuevoParrafo=document.createElement('p');
+    nuevoParrafo.id= 'resultado';
+
+        if(numeroAleatorio > numero){
+            nuevoParrafo.innerHTML = 'El numero es mayor que el ingresado. Intentalo de nuevo!';
+        }
+        if(numeroAleatorio < numero){
+            nuevoParrafo.innerHTML = 'El numero es menor que el ingresado. Intentalo de nuevo!';
+        }
+        if(numeroAleatorio == numero){
+            nuevoParrafo.innerHTML = 'ACERTASTE! 😁👍';
+            nuevoParrafo.className ='text-success'
+        }
+        div[2].appendChild(nuevoParrafo);
+ }
+}
+
 
